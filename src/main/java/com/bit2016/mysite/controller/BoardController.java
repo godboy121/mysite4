4c2dc2a748b628e9f9d5eece7f4da500edc9dc67
namdarwoo <namdarwoo@192.168.35.85>
2016-11-08 00:35:46 +0900

    SECOND

---
+++ src/main/java/com/bit2016/mysite/controller/BoardController.java
@@ -0,0 +1,33 @@
+package com.bit2016.mysite.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.bit2016.mysite.service.BoardService;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
+	@RequestMapping( "" )
+	public String index(
+		@RequestParam( value="p", required=true, defaultValue="1") Integer page,
+		@RequestParam( value="kwd", required=true, defaultValue="") String keyword,
+		Model model ) {
+		
+		Map<String, Object> map = 
+				boardService.getList( page, keyword );
+		
+		model.addAttribute( "map", map );
+		return "board/index";
+	}
+
+}
